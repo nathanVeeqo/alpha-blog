e7c68d09fe3ed146d954fedbdf5a4909b4759f83
NathanVeeqo <nathan@veeqo.com>
2018-01-15 15:08:19 +0000

    Add edit and index page, updtae and show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,30 @@
 class ArticlesController < ApplicationController
 
+def index
+@articles = Article.all
+end
+
 def new
 	@article = Article.new
 
 
 end
+
+def edit
+	@article = Article.find(params[:id])
+end
+
+def update
+	@article = Article.find(params[:id])
+	if @article.update(article_params)
+		flash[:notice] = "Article was successfully updated"
+		redirect_to article_path(@article)
+	else
+		render 'edit'
+	end
+
+end
+
 
 def create
 
@@ -24,6 +44,7 @@ def show
 @article = Article.find(params[:id])
 	
 end
+
 
 
 private 
